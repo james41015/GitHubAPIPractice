e0784c2607404588e905082f4ae751c85894ae72
JamesHong <james41015@gmail.com>
2019-07-26 15:59:20 +0800

    original

---
+++ GitHubAPIPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubAPIPractice
+//
+//  Created by Hong James on 2019/7/26.
+//  Copyright © 2019 Hong James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
